aab6aeea134caceacfcff5e34c11eb708759f545
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-12 23:54:31 +0300

    Adds logic in StatefulLogic

--- src/StatefulLogic/StatefulLogic.js
+++ src/StatefulLogic/StatefulLogic.js
@@ -1,5 +1,4 @@
-import React, { Component, Fragment } from 'react';
-import {getLoggedInUser} from '../utils'
+import React, { Component } from 'react';
 
 /*
   Render-props можно использовать для вынесения стейтфул логики
@@ -14,11 +13,31 @@ import {getLoggedInUser} from '../utils'
   Кнопка должна получать onClick коллбек из компонента-обёртки
 */
 
-export const WithTooltip = (onClick) => {
-  return (
-    <Fragment>
-      <div data-testid="tooltip">Hello, i'm Tooltip</div>
-      <button onClick={onClick}>I must have tooltip</button>
-    </Fragment>
-  );
-};
+export class WithTooltip extends Component {
+  state = {
+    isOpen: false
+  };
+
+  onClick = () => {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
+  render() {
+    const { render } = this.props;
+    const { isOpen } = this.state;
+    return <div>{render(isOpen, this.onClick)}</div>;
+  }
+}
+
+/* eslint-disable react/jsx-no-bind */
+export const AppComponent = () => (
+  <WithTooltip
+    render={(isOpen, onClick) => (
+      <div>
+        <button onClick={onClick}>I must have tooltip</button>
+        {isOpen && <div data-testid="tooltip">Hello, i'm Tooltip</div>}
+      </div>
+    )}
+  />
+);
